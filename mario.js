fd5938a48973226405a1cf7d517fd42a130a06bc
John Sirois <JPSirois@verizon.net>
2017-02-28 21:15:26 -0500

    Final working copy

--- mario.js
+++ mario.js
@@ -13,27 +13,28 @@ printPyramid(5);
  * #####
  *######
  */
-var height = Number(prompt("Enter a number for the height of the pyramid ", "1"));
 
-function printPyramid(height) {    
+function printPyramid(height) {
+    
 var h = height;
-/* loop through each layer of pyramid */    
+
+// Loop through each layer of pyramid    
 for(height; height>0; height -=1){
+	
+	// Define variables to hold spaces and bricks
     var space = "", brick = "";
-    /* Fill Spaces */ 
+
+    // Fill Spaces
     for(counter = 1; counter < height; counter += 1){
         space = space + " ";
     }
     
-    for(count = 0; count <= h-space.length; count += 1 ){
+	// Fill Bricks
+    for(counter = 0; counter <= h-space.length; counter += 1 ){
         brick = brick + "*";
-}
-    //for(space="_"; space.length < height; space = space + "_");
-    
-    /* Fill Bricks */
-    //for(brick="*"; brick.length <= (height-space.length); brick = brick + "*");
-    
+	}
+
+// Show the output    
 console.log(space + brick); 
 }
 }
-printPyramid(height);
